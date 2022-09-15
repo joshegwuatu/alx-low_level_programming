3f0e9631b56695f34edb4aa9ae6c102805d4237d
joshegwuatu <joshegwuatu@gmail.com>
2022-09-15 00:10:39 +0000

    3-islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10: - Check main
- * Description: funtion to print letters of the alphabet 10 times
+ * description: funtion to print letters of the alphabet 10 times
  * using _putchar
  * Return: Nothing
  */
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check main
+ * @c: An input character
+ * Description: function checks for lowercase letter
+ * Return: 1 if alphabet is lowercase and 0 if uppercase
+ */
+int _islower(int c)
+{
+	char i;
+	int lower =0;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i==c)
+			lower =1;
+
+	return (lower);
+}
