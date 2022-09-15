a119eb847c01834cb9662a895145b1047072bd6a
joshegwuatu <joshegwuatu@gmail.com>
2022-09-15 00:26:56 +0000

    4-isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,11 +9,11 @@
 int _islower(int c)
 {
 	char i;
-	int lower =0;
+	int lower = 0;
 
 	for (i = 'a'; i <= 'z'; i++)
-		if (i==c)
-			lower =1;
+		if ( i == c )
+			lower = 1;
 
 	return (lower);
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - check main
+ * @c: An input character
+ * Description: function checks for alphabet
+ * Return: 1 if letter is alphabet and 0 if not
+ */
+int _isalpha(int c)
+{
+	char i, j;
+	int alphabet = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z'; j++)
+			if ( c == i || c = j )
+				alphabet = 1;
+
+	return (alphabet);
+}
