613d14d43728cfebf38d262d65d9fe720ef58ea8
joshegwuatu <joshegwuatu@gmail.com>
2022-09-16 11:55:51 +0000

    Mor enumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers
+ * Description: function that prints numbers from 1 to 14 ten times
+ * Return: Nothing
+ */
+ 
+void more_numbers(void)
+{
+	char i;
+	int j = 0;
+	for (; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
