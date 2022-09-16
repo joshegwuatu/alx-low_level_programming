d5eb9a4910852c3b2f2991cd655a024589540cd1
joshegwuatu <joshegwuatu@gmail.com>
2022-09-16 13:10:00 +0000

    print diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal
+ *@n: lenght of line
+ * Description: function that prints line to n lenght diagonally
+ * Return: Nothing
+ */
+ 
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+												}
+	}
+	else
+		_putchar('\n');
+}
