ad8dd06f3ca1172e8abf6b17965f179b371eb743
joshegwuatu <joshegwuatu@gmail.com>
2022-09-27 03:13:28 +0000

    Pointers and array_string

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,24 +12,19 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a >= b && a >= c)
     {
         largest = a;
     }
-    else if (b > a && a > c)
+    else if (b >= a && b >= c)
     {
         largest = b;
     }
-    else if (a > b && a > c)
+    else if (c >= b && c >= b)
     {
-        largest = a;
+        largest = c;
     }
-    else if (b > a && b > c)
-    {
-	largest = b;
-    }
-    else
-	largest = c;
+    
 
     return (largest);
 }
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Project 0x07-pointers_arrays_strings
+
+This project has more pointers and arrays
